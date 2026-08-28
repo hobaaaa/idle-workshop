aad860f10dbbdf241405258452a7b4d716faa454
hobaaaa <denizgokbudak@gmail.com>
2026-08-29 01:41:45 +0300

    feat: add worker auto production system

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,22 +6,48 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private TMP_Text moneyText;
     [SerializeField] private TMP_Text productsText;
+    [SerializeField] private TMP_Text workersText;
     [SerializeField] private Button produceButton;
     [SerializeField] private Button sellButton;
     [SerializeField] private Button upgradeButton;
+    [SerializeField] private Button hireWorkerButton;
 
     private int money = 0;
     private int products = 0;
     private int productionPerClick = 1;
     private int sellPrice = 5;
     private int upgradeCost = 25;
+    private int workers = 0;
+    private int workerCost = 100;
+    private float workerTimer = 0f;
 
     private void Awake()
     {
         produceButton.onClick.AddListener(Produce);
         sellButton.onClick.AddListener(Sell);
         upgradeButton.onClick.AddListener(Upgrade);
+        hireWorkerButton.onClick.AddListener(HireWorker);
 
+        UpdateUI();
+    }
+
+    private void Update()
+    {
+        if (workers == 0)
+        {
+            return;
+        }
+
+        workerTimer += Time.deltaTime;
+
+        if (workerTimer < 1f)
+        {
+            return;
+        }
+
+        int elapsedSeconds = Mathf.FloorToInt(workerTimer);
+        products += workers * elapsedSeconds;
+        workerTimer -= elapsedSeconds;
         UpdateUI();
     }
 
@@ -30,6 +56,7 @@ public class GameManager : MonoBehaviour
         produceButton.onClick.RemoveListener(Produce);
         sellButton.onClick.RemoveListener(Sell);
         upgradeButton.onClick.RemoveListener(Upgrade);
+        hireWorkerButton.onClick.RemoveListener(HireWorker);
     }
 
     private void Produce()
@@ -63,10 +90,25 @@ public class GameManager : MonoBehaviour
         UpdateUI();
     }
 
+    private void HireWorker()
+    {
+        if (money < workerCost)
+        {
+            return;
+        }
+
+        money -= workerCost;
+        workers++;
+        workerCost = Mathf.CeilToInt(workerCost * 1.5f);
+        UpdateUI();
+    }
+
     private void UpdateUI()
     {
         moneyText.text = $"Money: ${money}";
         productsText.text = $"Products: {products}";
+        workersText.text = $"Workers: {workers}";
         upgradeButton.GetComponentInChildren<TMP_Text>().text = $"UPGRADE - ${upgradeCost}";
+        hireWorkerButton.GetComponentInChildren<TMP_Text>().text = $"HIRE WORKER - ${workerCost}";
     }
 }
